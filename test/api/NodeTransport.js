1fc3497d283cd27deb3aa61b0033c5f1cea91993
Henrique Vicente <henriquevicente@gmail.com>
2015-11-29 03:01:22 -0300

    Adding NodeTransport.

---
+++ test/api/NodeTransport.js
@@ -0,0 +1,50 @@
+'use strict';
+
+import NodeTransport from '../../src/api/NodeTransport';
+import ClientRequest from '../../src/api/ClientRequest';
+import RequestMock from '../RequestMock';
+
+var TransportRequestMock = RequestMock.get();
+
+describe('NodeTransport', function() {
+	beforeEach(TransportRequestMock.setup);
+	afterEach(TransportRequestMock.teardown);
+
+	it('should cancel send request to an url', function(done) {
+		TransportRequestMock.intercept('GET', 'http://localhost/url').reply(200);
+		var transport = new NodeTransport();
+		var clientRequest = new ClientRequest();
+		clientRequest.url('http://localhost/url');
+		transport.send(clientRequest)
+			.then(function() {
+				assert.fail();
+			})
+			.catch(function() {
+				// assert that connection.abort was called as well
+				done();
+			})
+			.cancel();
+	});
+
+	it('should cancel request if given timeout is reached', function(done) {
+		TransportRequestMock.intercept('GET', 'http://localhost/url?foo=1').socketDelay(5).reply(200);
+
+		var transport = new NodeTransport();
+		var clientRequest = new ClientRequest();
+		clientRequest.url('http://localhost/url?foo=1');
+		transport.request(
+			clientRequest.url(),
+			clientRequest.method(),
+			null,
+			null,
+			null,
+			1
+		).then(function() {
+			assert.fail();
+		})
+		.catch(function(e) {
+			assert.strictEqual('ESOCKETTIMEDOUT', e.code);
+			done();
+		});
+	});
+});
