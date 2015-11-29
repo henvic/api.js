8b7d849a60695ed43bbaa770021429d4e77b4834
Henrique Vicente <henriquevicente@gmail.com>
2015-11-29 02:55:15 -0300

    Adding build / test tasks for node. Improving karma configuration organization.

--- envs/node-test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import assert from 'assert';
-import NodeRequestMock from '../test/fixtures/NodeRequestMock';
-import RequestMock from '../test/fixtures/RequestMock';
-
-RequestMock.set(NodeRequestMock);
-
-global.assert = assert;
