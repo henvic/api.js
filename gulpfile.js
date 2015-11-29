def48d02d1ddafd058a76d564920434d2e0951a8
Henrique Vicente <henriquevicente@gmail.com>
2015-11-29 02:55:15 -0300

    Adding build / test tasks for node. Improving karma configuration organization.

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,56 @@ var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var runSequence = require('run-sequence');
 var metal = require('gulp-metal');
+var mocha = require('gulp-mocha');
 
-metal.registerTasks({
-	globalName: 'launchpad',
-	bundleFileName: 'api.js'
+require('babel-core/register')({
+	resolveModuleSource: metal.renameAlias,
+	sourceMap: 'both'
 });
+
+var options = {
+	globalName: 'launchpad',
+	bundleFileName: 'api.js',
+	buildSrc: ['src/**/*.js', '!src/**/*Transport.js']
+};
+
+var isNodeBuild = process.argv.indexOf('build:node') !== -1;
+
+if (isNodeBuild && process.argv.indexOf('build') !== -1) {
+	console.error('Can\'t run build and build:node at once.');
+	process.exit(1);
+}
+
+function setupEnv(type) {
+	if (type === 'browser') {
+		options.buildSrc.push('envs/browser.js');
+		options.buildDest = 'build/globals';
+		return;
+	}
+
+	options.buildSrc.push('envs/node.js');
+	options.buildDest = 'build/node';
+}
+
+setupEnv(isNodeBuild ? 'node' : 'browser');
+metal.registerTasks(options);
+
+function nodeTest() {
+	return gulp.src([
+		'bower_components/metal-promise/**/*.js',
+		'src/**/*.js',
+		'!src/api/AjaxTransport.js',
+		'envs/node.js',
+		'envs/NodeRequestMock.js',
+		'test/**/*.js',
+		'!test/api/AjaxTransport.js'])
+    .pipe(mocha({
+    	require: ['source-map-support/register']
+    }));
+}
+
+gulp.task('test:node', nodeTest);
+gulp.task('build:node', ['build:globals:js']);
 
 gulp.task('build:min', function() {
 	return gulp.src('build/api.js')
--- karma-coverage.conf.js
+++ karma-coverage.conf.js
@@ -1,45 +1,24 @@
+var common = require('./karma-common-settings');
 var isparta = require('isparta');
 var metal = require('gulp-metal');
 
-var babelOptions = {
-  resolveModuleSource: metal.renameAlias,
-  sourceMap: 'both'
-};
-
 module.exports = function (config) {
-  config.set({
-    frameworks: ['mocha', 'chai', 'sinon', 'source-map-support', 'commonjs'],
+	var settings = common.karma;
 
-    files: [
-      'bower_components/metal/**/*.js',
-      'bower_components/metal-promise/**/*.js',
-      'bower_components/soyutils/soyutils.js',
-      'src/**/*.js',
-      'test/**/*.js'
-    ],
+	settings.preprocessors['src/**/*.js'] = ['coverage', 'commonjs'];
 
-    preprocessors: {
-      'bower_components/metal/**/*.js': ['babel', 'commonjs'],
-      'bower_components/metal-promise/**/*.js': ['babel', 'commonjs'],
-      'src/**/*.js': ['coverage', 'commonjs'],
-      'test/**/*.js': ['babel', 'commonjs']
-    },
+	settings.reporters = ['coverage', 'progress'];
 
-    browsers: ['Chrome'],
+	settings.coverageReporter = {
+    instrumenters: {isparta : isparta},
+    instrumenter: {'**/*.js': 'isparta'},
+    instrumenterOptions: {isparta: {babel: common.babel}},
+    reporters: [
+      {type: 'html'},
+      {type: 'lcov', subdir: 'lcov'},
+      {type: 'text-summary'}
+    ]
+  };
 
-    reporters: ['coverage', 'progress'],
-
-    babelPreprocessor: {options: babelOptions},
-
-    coverageReporter: {
-      instrumenters: {isparta : isparta},
-      instrumenter: {'**/*.js': 'isparta'},
-      instrumenterOptions: {isparta: {babel: babelOptions}},
-      reporters: [
-        {type: 'html'},
-        {type: 'lcov', subdir: 'lcov'},
-        {type: 'text-summary'}
-      ]
-    }
-  });
+  config.set(settings);
 };
