46886043f056a1508c24b22a1a06dbf5e0fb9199
Henrique Vicente <henriquevicente@gmail.com>
2015-11-29 03:10:11 -0300

    Adding Node tests to npm test and Travis file.

---
+++ envs/browser.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import TransportFactory from '../src/api/TransportFactory';
+import AjaxTransport from '../src/api/AjaxTransport';
+import Filter from '../src/api-query/Filter';
+import Geo from '../src/api-query/Geo';
+import Query from '../src/api-query/Query';
+import Range from '../src/api-query/Range';
+import Launchpad from '../src/api/Launchpad';
+
+TransportFactory[TransportFactory.DEFAULT_TRANSPORT_NAME] = AjaxTransport;
+Launchpad.socket(window.io);
+
+window.Filter = Filter;
+window.Geo = Geo;
+window.Query = Query;
+window.Range = Range;
+window.Launchpad = Launchpad;
